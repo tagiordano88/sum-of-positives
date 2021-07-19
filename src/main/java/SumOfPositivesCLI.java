292f12854ec05091844d72a693a48507faf12a70
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-19 16:03:51 -0500

    working solution

--- src/main/java/SumOfPositivesCLI.java
+++ src/main/java/SumOfPositivesCLI.java
@@ -2,7 +2,10 @@ public class SumOfPositivesCLI {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 0; i < args.length; i++) {
-            System.out.println("It Works");
+            if (Integer.parseInt(args[i]) >= 0) {
+                sum += Integer.parseInt(args[i]);
+            }
         }
+        System.out.println(sum);
     }
 }
