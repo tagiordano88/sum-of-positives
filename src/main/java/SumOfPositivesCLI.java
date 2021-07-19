56bd3c9d962a994c4d094169098dcc0667e8bc8a
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-19 15:39:18 -0500

    got jar file to work

---
+++ src/main/java/SumOfPositivesCLI.java
@@ -0,0 +1,8 @@
+public class SumOfPositivesCLI {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("It Works");
+        }
+    }
+}
